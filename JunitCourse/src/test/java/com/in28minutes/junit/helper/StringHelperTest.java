28b9244e738d51dc2b242c6b5571cd57f321b865
swara12262 <swaranjalisawant@gmail.com>
2021-03-15 21:46:52 +0530

    Second Junit test case

--- JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -5,15 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
-
+	StringHelper sh=new StringHelper();
 	@Test
 	public void testTruncateAInFirst2Positions_1() {
-		StringHelper sh=new StringHelper();
+		
 		assertEquals("CD",sh.truncateAInFirst2Positions("AACD"));
 	}
 	@Test
 	public void testTruncateAInFirst2Positions_2() {
-		StringHelper sh=new StringHelper();
+		
 		assertEquals("CD",sh.truncateAInFirst2Positions("ACD"));
+	}
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame() {
+		assertFalse( sh.areFirstAndLastTwoCharactersTheSame("ABCD"));
 	}
 }
