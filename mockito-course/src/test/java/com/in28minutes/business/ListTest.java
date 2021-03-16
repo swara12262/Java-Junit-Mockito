e3dadefb228d2869ab7df72119eea418bad8442e
swara12262 <swaranjalisawant@gmail.com>
2021-03-16 13:58:04 +0530

    Differenct uses of Mockito

---
+++ mockito-course/src/test/java/com/in28minutes/business/ListTest.java
@@ -0,0 +1,47 @@
+package com.in28minutes.business;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListTest {
+
+	@Test
+	public void testMockListSizeMethod() {
+		List listmock = mock(List.class);
+		when(listmock.size()).thenReturn(2);
+		assertEquals(2,listmock.size());
+	}
+	
+	@Test
+	public void testMockListSizeMethod_ReturnMultipleValues() {
+		List listmock = mock(List.class);
+		when(listmock.size()).thenReturn(2).thenReturn(3);
+		assertEquals(2,listmock.size());
+		assertEquals(3,listmock.size());
+	}
+	
+	@Test
+	public void testMockListGetMethod() {
+		List listmock = mock(List.class);
+		when(listmock.get(0)).thenReturn("Swara");
+		assertEquals("Swara",listmock.get(0));
+		assertEquals(null,listmock.get(1));
+	}
+	
+	@Test(expected =RuntimeException.class)
+	public void testMockList_ThrowException() {
+		List listmock = mock(List.class);
+		when(listmock.get(anyInt())).thenThrow(new RuntimeException());
+		listmock.get(0);
+		
+	}
+	
+	
+
+}
